79bad260de103d2a7038b728f66cfb06c154e292
Beloved46 <belovedakande@gmail.com>
2023-11-24 16:46:24 +0100

    create subaccount for business on registering

--- app/Services/BusinessService.php
+++ app/Services/BusinessService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Business;
 use App\Models\BusinessType;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 
 class BusinessService
 {
@@ -12,7 +13,7 @@ class BusinessService
     {
         $businessType = BusinessType::where('name',$data->business_type)->first();
         
-        return Business::create([
+        $business =  Business::create([
         'name' => $data->name,
         'email' => $data->email,
         'phone' => $data->phone, 
@@ -22,6 +23,65 @@ class BusinessService
         'bank_name' => $data->bank_name,
         'business_type_id' => $businessType->id,
     
-    ]);
+        ]);
+
+        return $this->createPaystackSubAccount($business);
+        
+    }
+
+
+
+    protected function createPaystackSubAccount($business)
+    {
+        $url = "https://api.paystack.co/subaccount";
+
+       $bankCode = $this->getBankCode($business->bank_name);
+
+        $fields = [
+            'business_name' => $business->name,
+            'bank_code' => $bankCode, //bank code
+            'account_number' => $business->acct_number,
+            'percentage_charge' => 0.8
+        ];
+
+        $response = Http::withHeaders([
+                "Authorization" => env('PAYSTACK_SECRET_KEY'),
+                "Cache-Control" => "no-cache",
+            ])
+            ->post($url, $fields);
+
+        $result = $response->body();
+
+        return $result;
+
+    }
+
+
+    protected function getBankCode($bankName)
+    {
+        $response =  Http::get('https://nigerianbanks.xyz'); //api to get all banks in nigeria and thier codes
+
+        if ($response->successful()) {
+
+            $banks = $response->json(); 
+
+            $matchedBanks = array_filter($banks, function ($bank) use ($bankName) {
+                return stripos($bank['name'], $bankName) !== false;
+            });
+
+            if (!empty($matchedBanks)) {
+                $firstMatchedBank = reset($matchedBanks);
+
+                $bankCode = $firstMatchedBank['code'];
+                
+           
+                return $bankCode; 
+            } else {
+                return  response()->errorResonse('No bank found matching the search term.');
+            }
+        } else {
+            
+            return false;
+        }
     }
 }
